00356abc6851467cd6afe4909aa52cc9cda10206
Viktor Zhechev <33721248+ViktorZhechev88@users.noreply.github.com>
2018-07-10 21:03:08 +0300

    Add files via upload

---
+++ 2. Java Programming- Solving Problems with Software/Week 3/CSV Files and Basic Statistics in Java/FirstCSVExample.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of FirstCSVExample here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+public class FirstCSVExample {
+
+    public void readFood(){
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+        
+        for(CSVRecord record : parser){
+            System.out.print(record.get("Name") + "-");
+            System.out.print(record.get("Favorite Color") + "-");
+            System.out.println(record.get("Favorite Food"));
+        }
+    }
+}
